ec61ba5d5be2972c450fea62b4ec13ac4dc615f5
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-06-12 18:09:27 -0300

    Update Conta.java

--- src/com/company/orientacao_a_objetos/topicos_avancados/_6_4_tratando_e_lancando_excecoes/Conta.java
+++ src/com/company/orientacao_a_objetos/topicos_avancados/_6_4_tratando_e_lancando_excecoes/Conta.java
@@ -1,4 +1,4 @@
-package com.company.orientacao_a_objetos._6_topicos_avancados._6_4_tratando_e_lancando_excecoes;
+package com.company.orientacao_a_objetos.topicos_avancados._6_4_tratando_e_lancando_excecoes;
 
 public class Conta {
 
