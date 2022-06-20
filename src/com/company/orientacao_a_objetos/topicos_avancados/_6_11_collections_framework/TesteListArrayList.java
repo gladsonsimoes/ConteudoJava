ea56cae5dd5bd77f268154197c4344d36fdcf606
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-06-20 20:00:02 -0300

    Add files via upload

--- src/com/company/orientacao_a_objetos/topicos_avancados/_6_11_collections_framework/TesteListArrayList.java
+++ src/com/company/orientacao_a_objetos/topicos_avancados/_6_11_collections_framework/TesteListArrayList.java
@@ -10,17 +10,20 @@ public class TesteListArrayList {
         Aluno a3 = new Aluno("José");
         Aluno a4 = new Aluno("Pedro");
 
+
+        //aqui ele vai definir uma lista chamada "alunos", usando a Classe "Aluno"
         List<Aluno> alunos = new ArrayList<>();
 
-        alunos.add(a1);
+        alunos.add(a1); //aqui ele instancia a lista "alunos" , e adiciona o objeto "a1"
         alunos.add(a2);
         alunos.add(a3);
         alunos.add(a4);
         alunos.add(a4); //pode ser colocado repetido
 
-        imprimirAlunos(alunos);
+        imprimirAlunos(alunos); //metodo para imprimir alunos
     }
     private static void imprimirAlunos(List<Aluno> alunos){
+        //laço de repeticao para selecionar automaticamente os objetos 
         for (Aluno a : alunos ){
             System.out.println("Nome: " + a.getNome());
             }
