6ec1932a8b5d466caa5d054c1449c82005e63ee5
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-06-21 12:24:30 -0300

    Update Cargo.java

--- src/com/company/orientacao_a_objetos/topicos_avancados/_6_13_desafio_collections/Cargo.java
+++ src/com/company/orientacao_a_objetos/topicos_avancados/_6_13_desafio_collections/Cargo.java
@@ -1,4 +1,4 @@
-package com.company.orientacao_a_objetos.topicos_avancados._6_13_desafio_collections
+package com.company.orientacao_a_objetos.topicos_avancados._6_13_desafio_collections;
 
 import java.math.BigDecimal;
 
